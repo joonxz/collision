9beee71851631a34ac73647ef4b389c5cb38d475
Francis Vargas <fvargas213@gmail.com>
2015-04-01 14:49:06 -0400

    declared canvas and context

--- js/collision-test.js
+++ js/collision-test.js
@@ -1,21 +1,33 @@
-canvas.addEventListener("mousedown", function (e) {
 
-    for (var i = 0; i < spots.length; i++) {
-      // applied the pythagorean theorem
-      var dx = spots[i].x - mousePos.x;
-      var dy = spots[i].y - mousePos.y;
-      var distSquared = dx * dx + dy * dy;
-      var radiusSquared = spots[i].radius * spots[i].radius;
+(function () {
+  "use strict";
 
-      var drag = function () {
-        
-      }
+  // declare canvas
+  var canvas = document.getElementById('my-canvas');
+  var ctx = canvas.getContext("2d");
 
-      if (distSquared < radiusSquared) {
-        // spots[i].drag();
-        console.log("im in there");
-      } 
 
-    };
-    
-  }); 
+})();
+
+
+// canvas.addEventListener("mousedown", function (e) {
+
+//   for (var i = 0; i < spots.length; i++) {
+//     // applied the pythagorean theorem
+//     var dx = spots[i].x - mousePos.x;
+//     var dy = spots[i].y - mousePos.y;
+//     var distSquared = dx * dx + dy * dy;
+//     var radiusSquared = spots[i].radius * spots[i].radius;
+
+//     var drag = function () {
+      
+//     }
+
+//     if (distSquared < radiusSquared) {
+//       // spots[i].drag();
+//       console.log("im in there");
+//     } 
+
+//   };
+  
+// }); 
