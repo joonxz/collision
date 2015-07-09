84f9f2d0c568ce6a711ff8da810ccffaab748ccf
Francis Vargas <fvargas213@gmail.com>
2015-07-09 14:57:46 -0400

    limit size of shockwave - decrease opacity

--- js/shockwave.js
+++ js/shockwave.js
@@ -5,12 +5,14 @@ var Shockwave = function (x, y) {
   this.centerX = x;
   this.centerY = y;
 
-  this.radius = 10;
+  this.radius = player.radius - 10;
+  this.alpha = 1;
 };
 
 Shockwave.prototype.draw = function(ctx) {
   ctx.save();
   {
+    ctx.globalAlpha = this.alpha;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true); 
     ctx.lineWidth = 2; 
@@ -20,6 +22,14 @@ Shockwave.prototype.draw = function(ctx) {
   ctx.restore();
 };
 
-Shockwave.prototype.update = function(time) {
-  this.radius = this.radius + 4;
+Shockwave.prototype.update = function(time, ctx) {
+  // increases size of circle per frame 
+  this.radius = this.radius + 2;
+  // decreases opacity per frame
+  this.alpha = this.alpha - 0.04;
+  
+  // set alpha to 0 once the radius is a certain size
+  if (this.radius >= player.radius + 30) {
+    this.alpha = 0;
+  }
 };
