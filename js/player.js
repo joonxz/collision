5007beb5ff7b57f0776bedf0a49c4c27b2cb941e
Francis Vargas <fvargas213@gmail.com>
2015-07-09 14:35:50 -0400

    added player boundary

--- js/player.js
+++ js/player.js
@@ -12,5 +12,17 @@ var Player = function (x, y, radius, color) {
 Player.prototype = new Circle;
 
 Player.prototype.update = function(time) {
-  
+  // player boundary ---------
+  if (this.x + this.radius < this.radius * 2) {
+    this.x = this.radius;
+  } else if ( this.x - this.radius > canvasWidth - (this.radius * 2)) {
+    this.x = canvasWidth - this.radius;
+  }
+
+  if (this.y + this.radius < this.radius * 2) {
+    this.y = this.radius;
+  } else if ( this.y - this.radius > canvasHeight - (this.radius * 2)) {
+    this.y = canvasHeight - this.radius;
+  }
+  // -------------------------
 };
