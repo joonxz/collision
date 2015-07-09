55919fc04713a4f065b3a59e162c77baf5b1ebe8
Francis Vargas <fvargas213@gmail.com>
2015-07-09 15:33:17 -0400

    reworked Shockwave to only till a certain size

--- js/shockwave.js
+++ js/shockwave.js
@@ -7,29 +7,28 @@ var Shockwave = function (x, y) {
 
   this.radius = player.radius - 10;
   this.alpha = 1;
+  // this.visible = 
 };
 
 Shockwave.prototype.draw = function(ctx) {
   ctx.save();
   {
-    ctx.globalAlpha = this.alpha;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true); 
-    ctx.lineWidth = 2; 
-    ctx.strokeStyle = "white";
-    ctx.stroke();
+    if (this.radius <= player.radius + 50) {
+      ctx.globalAlpha = this.alpha;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true); 
+      ctx.lineWidth = 2; 
+      ctx.strokeStyle = "white";
+      ctx.stroke();
+    }
   }
   ctx.restore();
 };
 
-Shockwave.prototype.update = function(time, ctx) {
+Shockwave.prototype.update = function(time) {
   // increases size of circle per frame 
   this.radius = this.radius + 2;
+
   // decreases opacity per frame
-  this.alpha = this.alpha - 0.04;
-  
-  // set alpha to 0 once the radius is a certain size
-  if (this.radius >= player.radius + 30) {
-    this.alpha = 0;
-  }
+  this.alpha = this.alpha - 0.03;
 };
